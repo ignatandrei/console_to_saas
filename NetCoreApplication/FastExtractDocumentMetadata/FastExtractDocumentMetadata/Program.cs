9c4ddb575169e2b3faf8e4727b53cb037a08931d
Andrei Ignat <ignatandrei@yahoo.com>
2019-03-25 23:26:21 +0200

    find word documents

---
+++ NetCoreApplication/FastExtractDocumentMetadata/FastExtractDocumentMetadata/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace FastExtractDocumentMetadata
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter the folder with financial Word documents");
+            string folderWithWordDocs = Console.ReadLine();
+            var files = Directory.GetFiles(folderWithWordDocs, "*.docx");
+            Console.WriteLine($"processing {files?.Length} word documents");
+            foreach (var file in files)
+            {
+                Console.WriteLine($"start processing {file}");
+                //TODO: find metadata in files
+                Console.WriteLine($"end processing {file}");
+
+            }
+        }
+    }
+}
