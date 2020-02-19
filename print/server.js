4afe7f0a75ad13365bbc4c08c494ca2958fd75a8
ignatandrei <ignatandrei@yahoo.com>
2020-02-19 23:44:29 +0200

    exapnding book

---
+++ print/server.js
@@ -0,0 +1,13 @@
+'use strict';
+console.log(`start print`);
+
+var markdownpdf = require("markdown-pdf")
+
+var mdDocs = [
+	"Chapter01/readme.md", "Chapter02/readme.md"
+	]
+  , bookPath = "book.pdf"
+
+markdownpdf().concat.from(mdDocs).to(bookPath, function () {
+  console.log("Created", bookPath)
+})
