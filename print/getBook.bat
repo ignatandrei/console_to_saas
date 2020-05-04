be26d6ea2de20e01f8548c2ace46cf5c93cec665
ignatandrei <ignatandrei@yahoo.com>
2020-05-04 21:43:18 +0300

    back to basics - line of codes do not work

--- print/getBook.bat
+++ print/getBook.bat
@@ -1,5 +1,6 @@
 docker build -t ignatandrei/printsaas .. -f ./exportPDF.txt  
 docker run -d --name printsaas  ignatandrei/printsaas
 docker cp printsaas:/usr/src/book.pdf .
+docker cp printsaas:/usr/src/Chapter01/lines.md ../Chapter01/lines.md
 docker container kill printsaas
 docker container rm printsaas
