624066db85ed37e82df7a1f161fc415649263156
ignatandrei <ignatandrei@yahoo.com>
2020-10-11 17:02:06 +0300

    just the date for the version

--- print/running.js
+++ print/running.js
@@ -5,8 +5,8 @@ exports.header = {
     contents: function(pageNum, numPages) {
         var date = new Date();
         var str=date.getFullYear().toString() + pad2(date.getMonth() + 1) + pad2( date.getDate()) + pad2( date.getHours() ) + pad2( date.getMinutes() ) + pad2( date.getSeconds() ) ;
-
-        return "<small>Console To SAAS version " + str  +"</small>"
+        str=date.getFullYear().toString() + pad2(date.getMonth() + 1) + pad2( date.getDate());
+        return "<small>Console To SAAS version " + str  +"</small>";
     }
  };
 
